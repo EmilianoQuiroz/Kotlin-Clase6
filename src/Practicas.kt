4c5833bc1891788e1b9fa1e79801774e9ca7156d
Emiliano Quiroz <santiagoquiroz.dev@gmail.com>
2022-05-09 17:35:57 -0300

    Clase 6

--- src/Practicas.kt
+++ src/Practicas.kt
@@ -1,4 +1,19 @@
-fun main(){
+fun main() {
+//CAPTURA DE DATOS POR CONSOLA
+    println("Ingrese su nombre: ")
+    var nombre = readLine()//Captura un String y lo devuelve
+    //Tambien se puede escribir como readln()
+    println("Hola $nombre!")
 
-    
+    println("Ingrese su edad: ")
+    //var edad = readLine()!!.toInt()
+    //Usando readln() no necesitamos poner !!
+    var edad = readln()
+    println("Su edad es de $edad años.")
+
+    //Valores nulos
+    /*
+    Un valor nulo implica que una variable esta vacia
+    o que el valor no es valido
+    */
 }
